74e46d61e6c5de40f56a0b9c4ca57962ace109e1
Troy <tli3693@gmail.com>
2017-09-26 19:41:45 -0400

    Setup AngularJS code

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,102 +1,82 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var hostname = 'localhost';
-var port = 27017;
-var database = "myDB";
-var db = mongojs("mongodb://troy:troy123@" + hostname + ":" + port + "/" + database, ['Tasks']);
+var db = mongojs('mongodb://troy:troy123@localhost:27017/myDB', ['tasks']);
 
 // Get All Tasks
-router.get('/tasks', function(req, res, next) {
-    db.Tasks.find(function(err, tasks) {
-        if(err) {
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
             res.send(err);
-        } 
+        }
         res.json(tasks);
-    })
+    });
 });
 
-// Get Single Task By Id
-router.get('/task/:id', function(req, res, next) {
-    db.Tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
-        if(err) {
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
             res.send(err);
-        } 
+        }
         res.json(task);
-    })
+    });
 });
 
-// Get Single Task By Not Done
-router.get('/task/isDone/:isDone2', function(req, res, next) {
-    var isDone1 = req.params.isDone2;
-    db.Tasks.find({isDone: false}, function(err, tasks) {
-        if(err) {
-            res.send(err);
-        } 
-        console.log("DEBUG: " + req.params.isDone==true);
-        console.log("Found " + (req.params.isDone==true?"completed":"incomplete") + " tasks.");
-        console.log(tasks);
-        res.json(tasks);
-    })
-});
-
-// Save Task
-router.post('/task', function(req, res, next) {
-    if(err) {
-        res.send(err);
-    } 
+//Save Task
+router.post('/task', function(req, res, next){
     var task = req.body;
-    if(!task.title || (task.isDone + '')) {
+    if(!task.title || !(task.isDone + '')){
         res.status(400);
         res.json({
             "error": "Bad Data"
         });
     } else {
-        db.Tasks.save(task, function(err, task) {
-            if(err) {
+        db.tasks.save(task, function(err, task){
+            if(err){
                 res.send(err);
-            } 
+            }
             res.json(task);
         });
     }
 });
 
 // Delete Task
-router.delete('/task/:id', function(req, res, next) {
-    db.Tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
-        if(err) {
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
             res.send(err);
-        } 
+        }
         res.json(task);
-    })
+    });
 });
 
 // Update Task
-router.put('/task/:id', function(req, res, next) {
+router.put('/task/:id', function(req, res, next){
     var task = req.body;
     var updTask = {};
-
-    if(task.isDone) {
+    
+    if(task.isDone){
         updTask.isDone = task.isDone;
     }
-    if(task.Title) {
-        updTask.Title = task.Title;
+    
+    if(task.title){
+        updTask.title = task.title;
     }
-    if(!updTask) {
+    
+    if(!updTask){
         res.status(400);
         res.json({
-            "error": "Bad Data"
+            "error":"Bad Data"
         });
+    } else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
     }
-
-    else {
-        db.Tasks.update({_id: mongojs.ObjectId(req.params.id)}, updTask, {}, function(err, task) {
-            if(err) {
-                res.send(err);
-            } 
-            res.json(task);
-        })
-    }   
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-// paths refer to the .js files
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
 
@@ -10,7 +9,7 @@ var port = 3000;
 
 var app = express();
 
-// View Engine
+//View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -18,13 +17,13 @@ app.engine('html', require('ejs').renderFile);
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'client')));
 
-// Body Parse Middleware
+// Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
-app.use('/task_api', tasks);
+app.use('/api', tasks);
 
-app.listen(port, function() {
-    console.log('Server started on port ' + port + '.' );
+app.listen(port, function(){
+    console.log('Server started on port '+port);
 });
